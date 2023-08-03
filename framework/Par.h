653d2ac6e22227cecb1b8c3cae803c660eeea279
Asier Pereiro <asier.pereiro.castro@cern.ch>
2023-08-03 20:53:47 +0200

    Update parameters

---
+++ framework/Par.h
@@ -0,0 +1,35 @@
+#pragma once
+
+enum Par {
+  /**
+   * Particle properties
+   */
+  tau_Bs,
+  Dm_Bs,
+  DG_Bs,
+
+  tau_Du,
+  Dm_Du,
+  DG_Du,
+
+  mass_kst0892,
+  width_kst0892,
+
+  /**
+     Covariant amplitude coefficients
+   */
+  abs_VV_S,
+  arg_VV_S,
+  absLambda_VV_S,
+  argLambda_VV_S,
+
+  abs_VV_P,
+  arg_VV_P,
+  absLambda_VV_P,
+  argLambda_VV_P,
+
+  abs_VV_D,
+  arg_VV_D,
+  absLambda_VV_D,
+  argLambda_VV_D,
+};
