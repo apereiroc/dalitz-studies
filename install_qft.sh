52f9eb1e7e51b1a68adcbc7cca8e97dba8df107e
Asier Pereiro <asier.pereiro.castro@cern.ch>
2023-08-03 20:17:39 +0200

    Add qft++ install script

---
+++ install_qft.sh
@@ -0,0 +1,9 @@
+CURRENT_PATH=`pwd`
+
+mkdir -p $CONDA_PREFIX/external &&
+git clone https://github.com/jdalseno/qft.git $CONDA_PREFIX/external/qft &&
+cd $CONDA_PREFIX/external/qft  &&
+scons -j7 &&
+ln -sf $CONDA_PREFIX/external/qft/lib/libqft++.so $CONDA_PREFIX/lib &&
+
+cd $CURRENT_PATH
