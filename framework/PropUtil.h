772a2716b67e1b6389e19b5954045011de188840
Asier Pereiro <asier.pereiro.castro@cern.ch>
2023-08-04 18:00:52 +0200

    Add factory design pattern to implement propagator

--- framework/PropUtil.h
+++ framework/PropUtil.h
@@ -1,8 +1,19 @@
 #pragma once
 
+#include "MassModels.h"
 #include "Propagator.h"
+#include <iostream>
 #include <memory>
 
 enum PropConf { BW };
 
-inline std::shared_ptr<Propagator> NewPropagator(const PropConf &conf);
+inline std::shared_ptr<Propagator> NewPropagator(const PropConf &conf) {
+  switch (conf) {
+  case PropConf::BW:
+    return std::make_shared<class BreitWigner>();
+  default: {
+    std::cout << "ERROR: propagator not implemented" << std::endl;
+    std::exit(1);
+  }
+  }
+}
