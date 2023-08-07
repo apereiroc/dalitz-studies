1aaac13aafaad655bedd9106636676e7abb0bd2a
Asier Pereiro <asier.pereiro.castro@cern.ch>
2023-08-07 12:55:21 +0200

    Move abs/arg Lambda

--- framework/Par.h
+++ framework/Par.h
@@ -20,16 +20,19 @@ enum Par {
    */
   abs_VV_S,
   arg_VV_S,
-  absLambda_VV_S,
-  argLambda_VV_S,
 
   abs_VV_P,
   arg_VV_P,
-  absLambda_VV_P,
-  argLambda_VV_P,
 
   abs_VV_D,
   arg_VV_D,
+
+  absLambda_VV_S,
+  argLambda_VV_S,
+
+  absLambda_VV_P,
+  argLambda_VV_P,
+
   absLambda_VV_D,
   argLambda_VV_D,
 };
