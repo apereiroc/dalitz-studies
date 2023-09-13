c3ad3731ce5baf519ca2b29e83f8404891c8d854
Asier Pereiro <asier.pereiro.castro@cern.ch>
2023-09-13 15:55:23 +0200

    add scalar stuff

--- framework/Par.h
+++ framework/Par.h
@@ -1,38 +1,63 @@
 #pragma once
 
 enum Par {
-  /**
-   * Particle properties
-   */
-  tau_Bs,
-  Dm_Bs,
-  DG_Bs,
+    /**
+     * Particle properties
+     */
+    tau_Bs,
+    Dm_Bs,
+    DG_Bs,
 
-  tau_Du,
-  Dm_Du,
-  DG_Du,
+    tau_Du,
+    Dm_Du,
+    DG_Du,
 
-  mass_kst0892,
-  width_kst0892,
+    mass_kst892,
+    width_kst892,
 
-  /**
-     Covariant amplitude coefficients
-   */
-  abs_VV_S,
-  arg_VV_S,
+    mass_kst1430,
+    width_kst1430,
 
-  abs_VV_P,
-  arg_VV_P,
+    a_lass,
+    r_lass,
 
-  abs_VV_D,
-  arg_VV_D,
+    /**
+       Covariant amplitude coefficients
+     */
+    abs_VV_S,
+    arg_VV_S,
 
-  absLambda_VV_S,
-  argLambda_VV_S,
+    abs_VV_P,
+    arg_VV_P,
 
-  absLambda_VV_P,
-  argLambda_VV_P,
+    abs_VV_D,
+    arg_VV_D,
 
-  absLambda_VV_D,
-  argLambda_VV_D,
+    abs_VS_plus,
+    arg_VS_plus,
+
+    abs_VS_minus,
+    arg_VS_minus,
+
+    abs_SS,
+    arg_SS,
+
+
+    absLambda_VV_S,
+    argLambda_VV_S,
+
+    absLambda_VV_P,
+    argLambda_VV_P,
+
+    absLambda_VV_D,
+    argLambda_VV_D,
+
+    absLambda_VS_plus,
+    argLambda_VS_plus,
+
+    absLambda_VS_minus,
+    argLambda_VS_minus,
+
+    absLambda_SS,
+    argLambda_SS,
 };
